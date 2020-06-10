c2685f47a154e33a7280be052cfb9ae52b8ffc9e
ppxb <317842449@qq.com>
2020-06-10 14:52:49 +0800

    fix: fiex some bugs

--- admin/src/views/Dashboard.vue
+++ admin/src/views/Dashboard.vue
@@ -2,16 +2,11 @@
   <v-app id="keep">
     <v-app-bar app clipped-left color="amber">
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <span class="title ml-3 mr-5"
-        >Top&nbsp;<span class="font-weight-light">FullStack</span></span
-      >
-      <v-text-field
-        solo-inverted
-        flat
-        hide-details
-        label="Search"
-        prepend-inner-icon="mdi-magnify"
-      ></v-text-field>
+      <span class="title ml-3 mr-5">
+        Top&nbsp;
+        <span class="font-weight-light">FullStack</span>
+      </span>
+      <v-text-field solo-inverted flat hide-details label="Search" prepend-inner-icon="mdi-magnify"></v-text-field>
       <v-spacer></v-spacer>
     </v-app-bar>
 
@@ -20,25 +15,16 @@
         <template v-for="(item, i) in items">
           <v-row v-if="item.heading" :key="i" align="center">
             <v-col cols="6">
-              <v-subheader v-if="item.heading">
-                {{ item.heading }}
-              </v-subheader>
+              <v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
             </v-col>
           </v-row>
-          <v-divider
-            v-else-if="item.divider"
-            :key="i"
-            dark
-            class="my-4"
-          ></v-divider>
+          <v-divider v-else-if="item.divider" :key="i" dark class="my-4"></v-divider>
           <v-list-item v-else :key="i" link :to="item.path">
             <v-list-item-action>
               <v-icon>{{ item.icon }}</v-icon>
             </v-list-item-action>
             <v-list-item-content>
-              <v-list-item-title class="grey--text">
-                {{ item.text }}
-              </v-list-item-title>
+              <v-list-item-title class="grey--text">{{ item.text }}</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </template>
@@ -53,28 +39,28 @@
   </v-app>
 </template>
 
-<script>
-export default {
-  props: {
-    source: String
-  },
-  data: () => ({
-    drawer: null,
-    items: [
-      { icon: 'mdi-palette', text: 'Dashboard', path: '/' },
-      { divider: true },
-      { heading: 'Opertation' },
-      {
-        icon: 'mdi-radio-tower',
-        text: 'Graphql',
-        path: '/test'
-      },
-      { divider: true },
-      { heading: 'System' },
-      { icon: 'mdi-cog', text: 'Settings' },
-      { icon: 'mdi-police-badge', text: 'Auth' }
-    ]
-  })
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component({})
+export default class Dashboard extends Vue {
+  @Prop() source!: string
+
+  drawer!: any
+  items = [
+    { icon: 'mdi-palette', text: 'Dashboard', path: '/' },
+    { divider: true },
+    { heading: 'Opertation' },
+    {
+      icon: 'mdi-radio-tower',
+      text: 'Graphql',
+      path: '/test'
+    },
+    { divider: true },
+    { heading: 'System' },
+    { icon: 'mdi-cog', text: 'Settings' },
+    { icon: 'mdi-police-badge', text: 'Auth' }
+  ]
 }
 </script>
 
